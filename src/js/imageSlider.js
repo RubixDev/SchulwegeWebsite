41ebfcd6135dfac0e8e5a8446f88fdff1226b296
RubixDev <silas.groh@t-online.de>
2021-06-21 13:26:46 +0200

    Add imageSlider functionality

---
+++ src/js/imageSlider.js
@@ -0,0 +1,23 @@
+let imageIndex = 0
+
+function nextImage() {
+    const images = document.getElementById('imageSlider').children;
+    for (const image of images) {
+        image.style.opacity = 0
+    }
+    imageIndex++
+    if (imageIndex >= images.length) imageIndex = 0
+    images[imageIndex].style.opacity = 100
+    setTimeout(nextImage, 5000)
+}
+
+addLoadEvent(function () {
+    const imageSlider = document.getElementById('imageSlider')
+    for (let imageIndex = 1; imageIndex <= 7; imageIndex++) {
+        const image = document.createElement('img')
+        image.src = `assets/slideshow/0${imageIndex}.webp`
+        imageSlider.appendChild(image)
+    }
+})
+
+addLoadEvent(nextImage)
