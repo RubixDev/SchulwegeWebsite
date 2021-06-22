d0814135ffea1be978cdf760be46fdae36352f56
RubixDev <silas.groh@t-online.de>
2021-06-22 20:58:59 +0200

    Added images to cards

--- src/js/probleme.js
+++ src/js/probleme.js
@@ -12,12 +12,26 @@ function addMapMarker(x, y, title, text, imageSrc) {
 
     const markerCard = document.createElement('div')
     markerCard.className = 'markerCard'
-    markerCard.innerHTML = `<h3>${title}</h3><p>${text}</p>`
+    markerCard.innerHTML = `<div class="markerCardImg" style="background-image: url('../assets/map/${imageSrc}');"></div><h3>${title}</h3><p>${text}</p>`
     marker.appendChild(markerCard)
 
     map.appendChild(marker)
 }
 
 addLoadEvent(function () {
-    addMapMarker(18.7, 21.5, 'Carl-Fuhlrott Gymnasium', 'Unsere Schule', '')
+    addMapMarker(
+        18.7,
+        21.5,
+        'Fahrradständer am CFG',
+        'Obwohl sich am CFG bereits einige Fahrradständer befinden, scheinen diese oft, besonders an warmen Tagen, nicht auszureichen. ' +
+        'Außerdem befinden sie sich nur auf dem Osthof und nicht auf dem Westhof.',
+        'cfg-fahrradständer.jpg'
+    )
+    addMapMarker(
+        25.3,
+        25,
+        'Hahnerberg Kreuzung',
+        'Baustelle: Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum iusto reprehenderit consequatur quae aperiam optio voluptatum delectus aut ea ad.',
+        'hahnerberg.webp'
+    )
 })
