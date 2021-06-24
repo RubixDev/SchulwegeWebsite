e7b921643d7dbe163a0e249275d81634944df003
RubixDev <silas.groh@t-online.de>
2021-06-24 20:18:33 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-if [ -z "$1" ]; then
-  echo -e "\033[1;31mPlease specify a website root folder!\033[0m"
-  exit 1
+
+# rubixdev.github.io/SchulwegeWebsite
+echo -e "\033[1;36mPushing main branch src folder to gh-pages branch\033[0m"
+git subtree push --prefix src origin gh-pages
+echo -e "\033[33m..done\033[0m"
+
+# schulwege.rubixdev.de
+if [ "$USER" = "silas" ]; then
+    echo -e "\033[1;36mPulling gh-pages branch to schulwege.rubixdev.de\033[0m"
+    ssh contabo "cd /var/www/schulwege.rubixdev.de && git pull"
+    echo -e "\033[33m..done\033[0m"
 fi
-git subtree push --prefix $1 origin gh-pages
